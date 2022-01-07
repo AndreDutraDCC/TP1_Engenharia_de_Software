4a3e971cb0e2dfc396992712d65f10b1556df563
Isaac Lima <isaac.lima1@outlook.com>
2022-01-07 20:44:22 -0300

    added musixmatch api

--- API/handler.py
+++ API/handler.py
@@ -1,24 +1,22 @@
 import json
 
-
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
+def spotify(event, context):
+    """
+    Gives access to the Spotify API.
+    """
+    response = {}
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
+def musixmatch(event, context):
     """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
+    Gives access to the MusixMatch API.
     """
+    response = {}
+    return response
+
+def googlecloud(event, context):
+    """
+    Gives access to the Sentiments Reader Google Cloud API.
+    """
+    response = {}
+    return response
